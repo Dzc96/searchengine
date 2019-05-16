a60d3cea85ab65b2ab8a53eafbe307842817c650
Dzc96 <546448917@qq.com>
2019-05-16 16:38:25 +0800

    学校

--- src/main/java/com/gdei/searchengine/service/IndexServiceImpl.java
+++ src/main/java/com/gdei/searchengine/service/IndexServiceImpl.java
@@ -15,8 +15,9 @@ import java.nio.file.Paths;
 public class IndexServiceImpl implements IndexService{
 
     //这里不应该写死，应该弹出一个框让人选择
-    public static String indexDirectory = "C:\\Users\\EDZ\\Desktop\\index_directory";
+//    public static String indexDirectory = "C:\\Users\\EDZ\\Desktop\\index_directory";
 
+    public static String indexDirectory = "C:\\Users\\DELL\\Desktop\\index_directory";
     public void createIndex(String dataDirectory) throws Exception{
         Indexer indexer = new Indexer();
         Directory directory = FSDirectory.open(Paths.get(indexDirectory));
